c64f2a342349c89da06e3eb56459a8fb94807a00
Sindoval <sindoval321@gmail.com>
2026-01-26 16:40:00 -0300

    Implementacao da GlobalExceptionHandler

--- src/main/java/com/javanauta/usuario/infrastructure/security/JwtUtil.java
+++ src/main/java/com/javanauta/usuario/infrastructure/security/JwtUtil.java
@@ -4,38 +4,37 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import java.util.Date;
-import javax.crypto.SecretKey;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Service
 public class JwtUtil {
 
     // Chave secreta usada para assinar e verificar tokens JWT
-    private final SecretKey secretKey;
+    private final String secretKey = "sua_chave_secreta_super_segura_que_deve_ser_bem_longa";
 
-    public JwtUtil() {
-        this.secretKey = Keys.secretKeyFor((SignatureAlgorithm.HS256));
-    }
 
 
     // Gera um token JWT com o nome de usuário e validade de 1 hora
     public String generateToken(String username) {
         return Jwts.builder()
-                .setSubject(username) // Define o nome de usuário como o assunto do token
-                .setIssuedAt(new Date()) // Define a data e hora de emissão do token
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // Define a data e hora de expiração (1 hora a partir da emissão)
-                .signWith(secretKey) // Converte a chave secreta em bytes e assina o token com ela
-                .compact(); // Constrói o token JWT
+            .setSubject(username) // Define o nome de usuário como o assunto do token
+            .setIssuedAt(new Date()) // Define a data e hora de emissão do token
+            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // Define a data e hora de expiração (1 hora a partir da emissão)
+            .signWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS256) // Converte a chave secreta em bytes e assina o token com ela
+            .compact(); // Constrói o token JWT
     }
 
     // Extrai as claims do token JWT (informações adicionais do token)
     public Claims extractClaims(String token) {
         return Jwts.parser()
-                .setSigningKey(secretKey) // Define a chave secreta para validar a assinatura do token
-                .build()
-                .parseClaimsJws(token) // Analisa o token JWT e obtém as claims
-                .getBody(); // Retorna o corpo das claims
+            .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8))) // Define a chave secreta para validar a assinatura do token
+            .build()
+            .parseClaimsJws(token) // Analisa o token JWT e obtém as claims
+            .getBody(); // Retorna o corpo das claims
     }
 
     // Extrai o nome de usuário do token JWT
